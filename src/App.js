921c62c57c9935f4cef13b409a5baf60fc008bda
grigoryan0326 <grigoryan0326@mail.ru>
2024-10-05 04:01:49 +0400

    Published to Github Pages

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from "react"
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom"
+import { HashRouter as Router, Route, Routes } from "react-router-dom"
 import Header from "./components/Header/Header.jsx"
 import Home from "./components/Home/Home.jsx"
 import About from "./components/About/About.jsx"
